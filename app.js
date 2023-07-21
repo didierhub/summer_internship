8f6477f7d6f20789247d9833e0f2c7c88d407d29
didierhub <120401610+didierhub@users.noreply.github.com>
2023-07-21 20:06:30 +0000

    trying to make dynamic input block

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//log in page js
+
 let creat_account= document.getElementById('not_have_account');
 let existed_account=document.getElementById('have_account');
 
@@ -21,3 +23,25 @@ existed_account.addEventListener("click", function() {
       sign_up.style.display='none';
       log_in.style.display='block';
 })
+
+
+// add_input_fild_forth_block
+var survey_options = document.getElementsByClassName('new_fild_added');
+var add_more_fields = document.getElementById('add_more_fields');
+var remove_fields = document.getElementById('remove_fields');
+
+add_more_fields.addEventListener("click",function(){
+  var newField = document.createElement('input');
+  newField.setAttribute('type','text');
+  newField.setAttribute('name','survey_options[]');
+  newField.setAttribute('class','survey_options');
+  newField.setAttribute('placeholder','Another Field');
+  survey_options.appendChild(newField);
+} )
+
+remove_fields.onclick = function(){
+  var input_tags = survey_options.getElementsByTagName('input');
+  if(input_tags.length > 2) {
+    survey_options.removeChild(input_tags[(input_tags.length) - 1]);
+  }
+}
